e99a2a2b02afaf504447f5467e2fe16854d5bbe3
JPrzyborowski2000 <jakub.przyborowski2000@gmail.com>
2023-02-27 22:34:47 +0100

    TicTacToe270223

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,42 +1,36 @@
-import numpy as np
-
-game = [[1,1,2],
-		[0,2,1],
-		[2,2,0],]
-
-diags = []
-for ix in range(len(game)):
-	diags.append(game[ix][ix])
-
-for col, row, in zip(reversed(range(len(game))), range(len(game))):
-	print(col,row)
-	diags.append(game[row][col])
-
-print(diags)
-
-'''
-for col in range(len(game)):
-	check = []
-
-	for row in game:
-		check.append(row[col])
-
-	if check.count(check[0]) == len(check) and row[0] != 0:
-		print("Winner !")
-
-
+import itertools
 
 def win(current_game):
-	for row in game:
-		print(row)
+	#Horizontal
+	for row in game:	
 		if row.count(row[0]) == len(row) and row[0] != 0:
-			print("Winner !")
+			print(f"Player {row[0]} is the winner horizontally.")
 
-win(game)
+	#Diagonal
+	diags = []
+	for ix in range(len(game)):
+		diags.append(game[ix][ix])
+	
+	if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+		print(f"Player{diags[0]} is the winner diagonally (\\).")
 
-game = [[0,0,0],
-		[0,0,0],
-		[0,0,0],]
+	diags = []
+	for col, row, in zip(reversed(range(len(game))), range(len(game))):
+		diags.append(game[row][col])
+	
+	if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+		print(f"Player{diags[0]} is the winner diagonally(/).")
+
+	#Vertical
+	for col in range(len(game)):
+		check = []
+
+		for row in game:
+			check.append(row[col])
+
+		if check.count(check[0]) == len(check) and row[0] != 0:
+			print(f"Player{check[0]} is the winner vartically")
+
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
 	try:
@@ -53,5 +47,20 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
 		
 	return game_map
 
-game = game_board(game, player=1, row=2, column=1)
-''' 
+Play = True
+players = [1,2]
+while Play:
+	game = [[0,0,0],
+			[0,0,0],
+			[0,0,0],]
+
+	game_won = False
+	game = game_board(game, just_display=True)
+	while not game_won:
+		current_player = 1
+		column_chocie = int(input("What column do you want play? (0, 1, 2):"))
+		row_chocie = int(input("What row do you want play? (0, 1, 2):"))
+		game = game_board(game,current_player,row_chocie,column_chocie)
+
+
+#game = game_board(game, player=1, row=2, column=1)
