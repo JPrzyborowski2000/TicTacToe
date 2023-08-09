fcd89f8e0c3c246ceb812a19e92879cd92e8462c
JPrzyborowski2000 <102661173+JPrzyborowski2000@users.noreply.github.com>
2023-08-09 08:42:22 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,7 @@
 import itertools
 
 def win(current_game):
+	
 	#Horizontal
 	for row in game:	
 		if row.count(row[0]) == len(row) and row[0] != 0:
@@ -24,7 +25,7 @@ def win(current_game):
 	#Vertical
 	for col in range(len(game)):
 		check = []
-
+ 
 		for row in game:
 			check.append(row[col])
 
@@ -34,18 +35,25 @@ def win(current_game):
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
 	try:
+		if game_map[row][column] != 0:
+			print("This position is occupade ! Choose another !")
+			return game_map, False
 		print("   0  1  2")
 		if not just_display:
 			game_map[row][column] = player
 		for count,row in enumerate(game):
 			print(count,row)
+		return game_map, True
+
 	except IndexError as e:
 		print("Error: Did you input row/column as 0 1 or 2?",e)
+		return game_map, False
 	
 	except Exception as e:
 		print("Something go wrong !")
+		return game_map, False
 		
-	return game_map
+	
 
 Play = True
 players = [1,2]
@@ -55,12 +63,17 @@ while Play:
 			[0,0,0],]
 
 	game_won = False
-	game = game_board(game, just_display=True)
+	game,_  = game_board(game, just_display=True)
+	player_choice = itertools.cycle([1,2])
 	while not game_won:
-		current_player = 1
-		column_chocie = int(input("What column do you want play? (0, 1, 2):"))
-		row_chocie = int(input("What row do you want play? (0, 1, 2):"))
-		game = game_board(game,current_player,row_chocie,column_chocie)
+		current_player = next(player_choice)
+		print(f"Current PLayer: {current_player}")
+		played = False
+
+		while not played:
+			column_chocie = int(input("What column do you want play? (0, 1, 2):"))
+			row_chocie = int(input("What row do you want play? (0, 1, 2):"))
+			game, played = game_board(game,current_player,row_chocie,column_chocie)
 
 
 #game = game_board(game, player=1, row=2, column=1)
