4c93c756ca3d286b37ccdbd6994828661c6a4e45
JPrzyborowski2000 <102661173+JPrzyborowski2000@users.noreply.github.com>
2023-08-11 11:04:24 +0200

    Update TicTacToe.py
    Last edit from tutorial next will be my improve

--- TicTacToe.py
+++ TicTacToe.py
@@ -49,7 +49,7 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
 		if game_map[row][column] != 0:
 			print("This position is occupade ! Choose another !")
 			return game_map, False
-		print("   0  1  2")
+		print("   "+"  ".join([str(i) for i in range(len(game_map))]))
 		if not just_display:
 			game_map[row][column] = player
 		for count,row in enumerate(game):
@@ -68,13 +68,13 @@ Play = True
 players = [1,2]
 
 while Play:
-	game = [[0,0,0],
-			[0,0,0],
-			[0,0,0],]
-
+	
+	game_size = int(input("What size game of tic tac toe? "))
+	game = [[0 for i in range(game_size)] for i in range(game_size)]
 	game_won = False
 	game,_  = game_board(game, just_display=True)
 	player_choice = itertools.cycle([1,2])
+	
 	while not game_won:
 		current_player = next(player_choice)
 		print(f"Current PLayer: {current_player}")
@@ -87,7 +87,7 @@ while Play:
 
 		if win(game):
 			game_won = True
-			again = input("The game is over, would you like to play again? (y/n)")
+			again = int(input("The game is over, would you like to play again? (y/n)"))
 			if again.lower() == "y":
 				print("restatring")
 			elif again.lower() == "n":
